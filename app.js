e72d4b2fcce0341ba30ebc86fb9ae3ee54fc7f79
gixx98 <zsoltnagy98@gmail.com>
2020-06-16 21:27:08 +0200

    burger added for responsiveness
    added burger for mobile view

---
+++ app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //TOGGLE NAV
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+
+         // ANIMATE LINKS
+        navLinks.forEach((link, index) =>{
+            if(link.style.animation){
+                link.style.animation='';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+    });
+}
+
+const app = ()=>{
+    navSlide();
+}
+
+app();
+
